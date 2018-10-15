e6f9dc24d4257268f1d7072e01286430ec0a1f55
Johannes M. Griebler <johannes.griebler@gmail.com>
2018-10-15 19:46:16 +0200

    Add benchmarks for the other test programs

--- benches/example_programs.rs
+++ benches/example_programs.rs
@@ -41,5 +41,25 @@ fn bench_hello(c: &mut Criterion) {
     c.bench_function("hello", |b| b.iter(|| hello()));
 }
 
-criterion_group!(benches, bench_hello);
+fn bench_quine(c: &mut Criterion) {
+    fn quine() -> i32 {
+        let code = from_file!("quine.b98");
+
+        run(code)
+    }
+
+    c.bench_function("quine", |b| b.iter(|| quine()));
+}
+
+fn bench_fibo(c: &mut Criterion) {
+    fn fibo() -> i32 {
+        let code = from_file!("fibo.b98");
+
+        run(code)
+    }
+
+    c.bench_function("fibo", |b| b.iter(|| fibo()));
+}
+
+criterion_group!(benches, bench_hello, bench_quine, bench_fibo);
 criterion_main!(benches);
